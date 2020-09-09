1beb2da0648a363903e9cffe83b4b23709a56dac
adard2002 <adard2002@gmail.com>
2020-09-09 16:25:23 -0500

    trying to get slides to work

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ allow to take the today variable and output to page.
 document.write(today);
 */
 
-// slideshow js
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
 
 
 
