4bc3c7b731ada8ca6435515b777cdd7c50a5dead
adard2002 <adard2002@gmail.com>
2020-09-08 23:19:59 -0500

    adding the js for the slideshow

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 'use strict';
-
-
-
 console.log('This file is connected and ready to run.');
+
+/** This is from Class 6 Start, wrap up of 101 js.
+new is a keyword object that has properties, the date.
+var today = new Date();
+var is a keyword that makes the word today a variable.
+allow to take the today variable and output to page. 
+document.write(today);
+*/
+
+// slideshow js
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
+
