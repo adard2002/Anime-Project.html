22075286f10c9545011c4282dfdda647910374b7
adard2002 <adard2002@gmail.com>
2020-09-09 21:21:24 -0500

    test

--- app.js
+++ app.js
@@ -54,3 +54,33 @@ function showSlides() {
   setTimeout(showSlides, 4000); 
 }
 //end of slideshow js
+
+var userName = prompt('Please enter your first name.');
+alert('Hello, ' + userName + ' click OK to proceed.');
+
+var hourNow = prompt('What time is it, friend? Use Military time.');
+
+var greeting;
+
+if(hourNow >= 0 &&hourNow < 7){
+    greeting = 'Good Morning, ' + userName;
+} else if (hourNow >= 7 && hourNow <= 12){
+    greeting = 'Good mid day. ' + userName;
+} else if (hourNow > 12 && hourNow < 18){
+    greeting = 'Good afternoon ' + userName;
+} else if (hourNow >= 18 && hourNow <= 24){
+    greeting = 'Good Evening ' + userName;
+} else {
+    'Something went wrong!';
+}
+
+document.write('h1 style="color:yellow">' + greeting + '</h1>');
+
+var header = document.getElementById('removeThis');
+header.remove();
+
+
+
+
+
+
