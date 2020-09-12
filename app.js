da05ac5db2897bd2f779acb13e19b8f3ca1c86fe
adard2002 <adard2002@gmail.com>
2020-09-11 22:11:51 -0500

    making the corrections

--- app.js
+++ app.js
@@ -1,52 +1,6 @@
 'use strict';
 console.log('This file is connected and ready to run.');
 
-
-// slideshow js
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-//automatic slideshow 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); 
-}
-//end of slideshow js
-
 //The alert box that asks for your first name and the time (military time)
 var userName = prompt('Please enter your first name.');
 alert('Hello, ' + userName + ' click OK to proceed.');
@@ -75,6 +29,11 @@ header.remove();
 
 //* some reason when I reload the page the slideshow doesn't show up so you have click on either one of the arrows or one of the dots.
 //* I also wasnt able to do the same thing as Craig did in the video; having a little alert or popup box that asked you for your name and the time. 
+
+
+
+
+
 
 
 
