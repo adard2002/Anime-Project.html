133edf08b6294cfcc0278f462c7b502c48fafc9c
adard2002 <adard2002@gmail.com>
2020-09-14 00:27:43 -0500

    fixed the prompt timeout. thank you!

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
 'use strict';
-console.log('This file is connected and ready to run.');
 
-//The alert box that asks for your first name and the time (military time)
-var userName = prompt('Please enter your first name.');
-alert('Hello, ' + userName + ' click OK to proceed.');
 
-var hourNow = prompt('What time is it, friend? Use Military time.');
+function popupTimer() {
+  var dateFromStorage = localStorage.getItem('time');
+  var currentTime = new Date();
 
-var greeting;
+  if(dateFromStorage != null) {
+    var previousTime = new Date(dateFromStorage);
+    var secondsBetween = (currentTime.getTime() - previousTime.getTime()) / 1000;
 
-if(hourNow >= 0 &&hourNow < 7){
-    greeting = 'Good Morning, ' + userName;
-} else if (hourNow >= 7 && hourNow <= 12){
-    greeting = 'Good mid day. ' + userName;
-} else if (hourNow > 12 && hourNow < 18){
-    greeting = 'Good afternoon ' + userName;
-} else if (hourNow >= 18 && hourNow <= 24){
-    greeting = 'Good Evening ' + userName;
-} else {
-    'Something went wrong!';
+    console.log("It has been " + secondsBetween + " seconds since your last refresh.");
+  } else {
+    var userName = prompt('Please enter your first name.');
+    displayGreeting(userName, currentTime);
+  }
+  localStorage.setItem('time', currentTime);
 }
 
-//function myFunction() {
-  //  greeting = setTimeout(function(){ alert('Hello, ' + userName + ' click OK to proceed.') }, 1.8e+7);
-//}
+function displayGreeting(userName, currentTime) {
+  var hourNow = currentTime.getHours();
+  var greeting;
+  if(hourNow >= 0 &&hourNow < 7){
+      greeting = 'Good Morning, ' + userName;
+  } else if (hourNow >= 7 && hourNow <= 12){
+      greeting = 'Good mid day. ' + userName;
+  } else if (hourNow > 12 && hourNow < 18){
+      greeting = 'Good afternoon ' + userName;
+  } else if (hourNow >= 18 && hourNow <= 24){
+      greeting = 'Good Evening ' + userName;
+  } else {
+      'Something went wrong!';
+  }
 
-//function myStopFunction() {
-    //clearTimeout(greeting);
-//}
+  document.getElementById('greeting-placeholder').innerHTML = '<h2 style="color:#b5bcff">' + greeting + '</h2>';
+}
 
-document.write('<h2 style="color:#b5bcff">' + greeting + '</h2>');
+popupTimer();
 
-    var header = document.getElementById('removeThis');
+
+
+
+
+
+
+
 
 
 //* I was trying to get it so the popup prompt and alerts wouldn't pop up every time you refresh the page and instead only show it every 5 hours.
 //* I also wanted a particle effect to be the cursor trail and have the particles fall. 
-
-
-
 
 
 
