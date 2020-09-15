68321dd16e2be98587b3a4d496946d0306d927b5
adard2002 <adard2002@gmail.com>
2020-09-15 00:45:04 -0500

    adding the store files and found images of anime posters

---
+++ store.js
@@ -0,0 +1,20 @@
+//counts from 1 up to 6; there are 6 animes on the site so there are going to be 1 poster from each anime making 6 posters
+for(var i = 1; i < 7; i++) {
+    console.log('the number is currently at: ' + i)
+}
+
+var stop = '0'   
+
+while(true) {
+    var input = prompt('Give a number 1 through 6. Type 0 to exit.');
+    if(input === stop){
+        break;
+    }
+} 
+
+
+
+
+
+
+
