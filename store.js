8eb4349adf6f17d7ed250e5ae8076228dd82d559
adard2002 <adard2002@gmail.com>
2020-09-15 06:52:31 -0500

    couldnt figure out how to get the js to work yet

--- store.js
+++ store.js
@@ -1,19 +1,27 @@
-//counts from 1 up to 6; there are 6 animes on the site so there are going to be 1 poster from each anime making 6 posters
-for(var i = 1; i < 7; i++) {
-    console.log('the number is currently at: ' + i)
-}
-
-var stop = '0'   
-
-while(true) {
-    var input = prompt('Give a number 1 through 6. Type 0 to exit.');
-    if(input === stop){
-        break;
-    }
-} 
 
 
+var images = ['merchimages/DemonSlayerPoster.jpg', 'merchimages/AkameGaKillPoster.jpg', 'merchimages/DarlingintheFranxxPoster.jpg', 'merchimages/AngelsofDeathPoster.jpg', 'merchimages/ServampPoster.jpg', 'merchimages/OwarinoseraphPoster.jpg']; // array of pictures
+var stop = '0';
 
+
+    while(true){
+
+        var imput = prompt('Give a number 1 through 6. Type 0 to exit.');
+    
+        if(input === stop){
+            break;
+        }
+        else{
+         
+           if (!IsNaN(input) && images.length >= input)
+              showImage(<img src="merchimages/DemonSlayerPoster.jpg"></img>, [input - 1]); 
+              showImage(<img src="merchimages/AkameGaKillPoster.jpg"></img>, [input - 2]);
+              showImage(<img src="merchimages/DarlingintheFraxxPoster.jpg"></img>, [input - 3]);      
+              showImage(<img src="merchimages/AngelsofDeathPoster.jpg"></img>, [input - 4]);
+              showImage(<img src="merchimages/ServampPoster.jpg"></img>, [input - 5]);
+              showImage(<img src="merchimages/OwarinoseraphPoster.jpg"></img>, [input - 6]); 
+        }
+    } 
 
 
 
