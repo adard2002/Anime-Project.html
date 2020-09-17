0af2bbe2300b5f9c882fc0bbd668901be0ca0075
adard2002 <adard2002@gmail.com>
2020-09-16 23:30:38 -0500

    fixed the scroll bar and blew up images when u hover over them on le home page

--- JS/rainbowprogressbar.js
+++ JS/rainbowprogressbar.js
@@ -1,7 +1,20 @@
-let totalHeight = document.body.scrollHeight -
-    window.innerHeight;
+//let totalHeight = document.body.scrollHeight -
+    ///window.innerHeight;
+
+//window.onscroll = function () {
+    //let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    //document.getElementById('progressbar').style.height = progressHeight + "%";
+//}
+
 
 window.onscroll = function () {
-    let progressHeight = (window.pageYOffset / totalHeight) * 100;
-    document.getElementById('progressbar').style.height = progressHeight + "%";
+
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressbar").style.height = scrolled + "%";
+
+    // Testing something.
+    // let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    // document.getElementById("progressbar").style.height = progressHeight + "%";
 }
